280b909c1e5b7ed9eff8721c82581ae73c638b57
Ashwin Dinesh <ashwin.dinesh@mirraw.com>
2017-01-28 12:57:13 +0530

    Created Android Custom Grid View project

---
+++ app/src/main/java/com/example/ashwin/customgridview/MainActivity.java
@@ -0,0 +1,56 @@
+package com.example.ashwin.customgridview;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.GridView;
+import android.widget.Toast;
+
+public class MainActivity extends Activity {
+    GridView grid;
+    String[] web = {
+            "Photos",
+            "Drive",
+            "Google Play",
+            "Chrome",
+            "Google Now",
+            "LinkedIn",
+            "Google+",
+            "Allo",
+            "Google Duo",
+            "Analytics"
+    } ;
+
+    int[] imageId = {
+            R.drawable.image1,
+            R.drawable.image2,
+            R.drawable.image3,
+            R.drawable.image4,
+            R.drawable.image5,
+            R.drawable.image6,
+            R.drawable.image7,
+            R.drawable.image8,
+            R.drawable.image9,
+            R.drawable.image10
+    };
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        CustomGrid adapter = new CustomGrid(MainActivity.this, web, imageId);
+        grid=(GridView)findViewById(R.id.grid);
+        grid.setAdapter(adapter);
+        grid.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Toast.makeText(MainActivity.this, "You Clicked at " +web[+ position], Toast.LENGTH_SHORT).show();
+            }
+        });
+
+    }
+
+}
